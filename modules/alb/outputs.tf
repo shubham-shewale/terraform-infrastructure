be45fc52d5806981b11093c90214fbffe19c1c14
Shubham Shewale <Shubham.Shewale@mastercard.com>
2025-08-25 21:25:50 +0530

    Updated alb output

--- modules/alb/outputs.tf
+++ modules/alb/outputs.tf
@@ -6,3 +6,12 @@ output "alb_dns_name" {
 output "target_group_arn" {
   value = aws_lb_target_group.web.arn
 }
+output "dns_name" {
+  description = "The DNS name of the load balancer"
+  value       = aws_lb.main.dns_name
+}
+
+output "zone_id" {
+  description = "The zone ID of the load balancer"
+  value       = aws_lb.main.zone_id
+}
