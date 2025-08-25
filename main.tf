65810fd0443bcf3101a716d599265595c270136d
Shubham Shewale <Shubham.Shewale@mastercard.com>
2025-08-25 10:33:14 +0530

    resolved typo

--- main.tf
+++ main.tf
@@ -318,7 +318,7 @@ resource "aws_route53_record" "cert_validation" {
   for_each = {
     for dvo in aws_acm_certificate.main.domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
-      record = dbo.resource_record_value
+      record = dvo.resource_record_value
       type   = dvo.resource_record_type
     }
   }
